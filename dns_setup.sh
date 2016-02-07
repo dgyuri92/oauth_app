36275af09024959b31461d3a649dd15d061aa112
György Demarcsek <dgy.jr92@gmail.com>
2016-02-07 23:32:13 +0100

    dns_setup.sh: Better regex for matching hostnames in docker-compose.yml

--- dns_setup.sh
+++ dns_setup.sh
@@ -20,7 +20,7 @@ fi
 echo "Docker machine: $docker_machine_ip"
 
 if [[ ! -z $docker_machine_ip ]]; then
-  hostnames=$(cat docker-compose.yml | grep 'hostname' | cut -d':' -f2)
+  hostnames=$(cat docker-compose.yml | grep '[^#]\+hostname:[\.a-zA-Z0-9 _\-]*' | cut -d':' -f2)
   line="${docker_machine_ip} ${hostnames}"
   sed "s/^$docker_machine_ip //g" /etc/hosts > /tmp/._new_hosts_file
   echo $line >> /tmp/._new_hosts_file
